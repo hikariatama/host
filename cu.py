4c135589490d7ab21566b66a9b616145350b58c2
Dan <innocoffee@ya.ru>
2021-10-31 02:12:17 +0300

    upload

---
+++ cu.py
@@ -0,0 +1,65 @@
+
+ 
+from telethon import functions, types 
+from .. import loader, utils 
+from asyncio import sleep 
+from telethon.tl.functions.account import UpdateProfileRequest 
+from telethon.tl.functions.users import GetFullUserRequest 
+ 
+@loader.tds 
+class CuMod(loader.Module): 
+    """Полное копирование юзера(ава, имя|фамилия, био)""" 
+    strings = {'name': 'Cu'} 
+    @loader.owner 
+    async def cucmd(self, message): 
+        """.cu <s> <a> <reply/@username> 
+        <s> - Скрытый режим 
+        <a> - Удалить ваши аватарки 
+        Аргументы после юзера не указывайте, не скушает 
+        Примеры: 
+        .cu s @user/reply 
+        .cu a @user/reply 
+        .cu s a @user/reply""" 
+        reply = await message.get_reply_message() 
+        user = None 
+        s = False 
+        a = False 
+        if utils.get_args_raw(message): 
+            args = utils.get_args_raw(message).split(" ") 
+            for i in args: 
+                if "s" == i.lower(): s = True 
+                elif "а" == i.lower() or "a" == i.lower(): a = True 
+                else: 
+                    try: 
+                        user = await message.client.get_entity(i) 
+                        break 
+                    except: 
+                        continue 
+        if user == None and reply != None: user = reply.sender 
+        if user == None and reply == None: 
+            if not s: await message.edit("Кого?") 
+            return 
+        if s: await message.delete() 
+        if not s: 
+            for i in range(0,11): 
+                await message.edit(f"Получаем доступ к аккаунту пользователя [{i*10}%]\n[{(i*'#').ljust(10, '–')}]") 
+                await sleep(0.3) 
+        if a: 
+            avs = await message.client.get_profile_photos('me') 
+            if len(avs) > 0: 
+                await message.client(functions.photos.DeletePhotosRequest(await message.client.get_profile_photos('me'))) 
+        full = await message.client(GetFullUserRequest(user.id)) 
+        if not s: await message.edit("Получаем аватарку... [35%]\n[###–––––––]") 
+        if full.profile_photo: 
+            up = await message.client.upload_file(await message.client.download_profile_photo(user, bytes)) 
+            if not s: await message.edit("Ставим аватарку... [50%]\n[#####–––––]") 
+            await message.client(functions.photos.UploadProfilePhotoRequest(up)) 
+        if not s: await message.edit("Получаем данные...  [99%]\n[#########–]") 
+        await message.client(UpdateProfileRequest( 
+            user.first_name if user.first_name != None else "", 
+            user.last_name if user.last_name != None else "", 
+            full.about[:70] if full.about != None else "" 
+        )) 
+        if not s: await message.edit("Аккаунт клонирован! [100%]\n[##########]") 
+        if not s: await sleep(5) 
+        if not s: await message.edit("Аккаунт клонирован!")
