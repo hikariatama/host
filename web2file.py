0e49c2959a9747a491a96bd7704fbf528aad24fc
innocoffee-ftg <36935426+innocoffee-ftg@users.noreply.github.com>
2021-11-03 09:08:26 +0300

    Upload file

---
+++ web2file.py
@@ -0,0 +1,42 @@
+"""
+    Copyright 2021 t.me/innocoffee
+    Licensed under the Apache License, Version 2.0
+    
+    Author is not responsible for any consequencies caused by using this
+    software or any of its parts. If you have any questions or wishes, feel
+    free to contact Dan by sending pm to @innocoffee_alt.
+"""
+
+#<3 title: Web2file
+#<3 pic: https://img.icons8.com/fluency/48/000000/archive.png
+#<3 desc: Скачивает контент из ссылки и отправляет в виде файла
+
+
+from .. import loader, utils
+import io
+import requests
+
+@loader.tds
+class Web2fileMod(loader.Module):
+	"""Скачивает контент из ссылки и отправляет в виде файла"""
+	strings = {'name': 'Web2file',
+	'no_args': '🦊 <b>Укажи ссылку</b>',
+	'fetch_error': '🦊 <b>Ошибка скачивания файла</b>',
+	'loading': '🦊 <b>Загрузка...</b>'}
+
+	async def web2filecmd(self, message):
+		""".web2file <ссылка на сайт> - Запаковать файл из сайта в файл"""
+		website = utils.get_args_raw(message)
+		if not website:
+			await utils.answer(message, self.strings('no_args', message))
+			return
+		try:
+			f = io.BytesIO(requests.get(website).content)
+		except:
+			await utils.answer(message, self.strings('fetch_error', message))
+			return
+
+		f.name = website.split('/')[-1]
+
+		await message.respond(file=f)
+		await message.delete()
