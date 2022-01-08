ffcf94de5c860ef731ad72d7124f67f7dddba7f0
Dan <36935426+innocoffee-ftg@users.noreply.github.com>
2022-01-08 20:55:17 +0300

    Upload file

---
+++ valutes.py
@@ -0,0 +1,26 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from .. import loader, utils
+import asyncio
+
+def register(cb):
+    cb(ValuteMod())
+class ValuteMod(loader.Module):
+    """Valute Converter"""
+    strings = {'name': 'Valute'}
+    async def valcmd(self, message):
+        """<sum> <valute> - Get exchange"""
+        state = utils.get_args_raw(message)
+        await utils.answer(message, "<b>Данные получены</b>")
+        chat = '@exchange_rates_vsk_bot'
+        async with message.client.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(events.NewMessage(incoming=True, from_users=1210425892))
+                bot_send_message = await message.client.send_message(chat, format(state))
+                bot_response = response = await response
+            except YouBlockedUserError:
+                await utils.answer(message, '<b>Убери из ЧС:</b> ' + chat)
+                return
+            await bot_send_message.delete()
+            await utils.answer(message, response.text)
+            await bot_response.delete()
