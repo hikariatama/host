cefcb981ed9178f0b3901426fc539ecda2391008
Dan <innocoffee@ya.ru>
2022-02-27 16:27:18 +0300

    upd

--- moduleslink.py
+++ moduleslink.py
@@ -4,32 +4,32 @@ from .. import loader, utils
  
 @loader.tds 
 class ModulesLinkMod(loader.Module): 
-    """Ссылка на модуль""" 
+    """Retrieves already installed modules' links""" 
     strings = {'name': 'ModulesLink'} 
  
-    async def mlcmd(self, message): 
-        """Вывести ссылку на модуль""" 
-        args = utils.get_args_raw(message) 
+    async def mlcmd(self, message):
+        """Send module link""" 
+        args = utils.get_args_raw(message)
         if not args: 
-            return await message.edit('Нет аргументов.') 
- 
-        await message.edit('Ищем...') 
- 
+            return await utils.answer(message, '🚫 <b>No args</b>') 
+
         try: 
-            f = ' '.join([x.strings["name"] for x in self.allmodules.modules if args.lower() == x.strings["name"].lower()]) 
+            f = ' '.join([x.strings["name"] for x in self.allmodules.modules if args.lower() == x.strings["name"].lower()])
             r = inspect.getmodule(next(filter(lambda x: args.lower() == x.strings["name"].lower(), self.allmodules.modules))) 
- 
-            link = str(r).split('(')[1].split(')')[0] 
+
+            link = str(r).split('(')[1].split(')')[0]
             if "http" not in link: 
-                text = f"Модуль {f}:" 
+                text = f"<b>🧳 {utils.escape_html(f)}</b>" 
             else: 
-                text = f"<a href=\"{link}\">Ссылка</a> на 🇺🇦{f}: <code>{link}</code>" 
- 
-            out = io.BytesIO(r.__loader__.data) 
-            out.name = f + ".py" 
+                text = f"<b><a href=\"{link}\">Link</a> for {utils.escape_html(f)}:</b> <code>{link}</code>" 
+
+            out = io.BytesIO(r.__loader__.data)
+            out.name = f'{f}.py'
             out.seek(0) 
- 
-            await message.respond(text, file=out) 
-            await message.delete() 
+
+            await message.respond(text, file=out)
+            
+            if message.out:
+                await message.delete()
         except: 
-            return await message.edit("Попробуй ввести имя модуля в кавычках")
+            await utils.answer(message, "😔 <b>Module not found</b>")
